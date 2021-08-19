52d384dc26370dd667a42adda3f211a74d6c9a36
alirezaImani-f4L3e <83541857+alirezaImani-f4L3e@users.noreply.github.com>
2021-08-19 12:58:40 +0430

    adding cors middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const data = require('./dataBase.json')
 
 const app = express();
+
+app.use(cors());
 
 app.get('/api/countries', (req, res) => {
 
